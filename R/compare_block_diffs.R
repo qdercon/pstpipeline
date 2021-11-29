8da0af2e19c6d200e55f75b635177110653a3433
qdercon <quentindercon@gmail.com>
2021-11-29 16:31:30 +0000

    scramble ids

--- R/compare_block_diffs.R
+++ R/compare_block_diffs.R
@@ -3,6 +3,8 @@
 #' @param all_res List output from [import_multiple()], split by variable of interest.
 #' @param var_of_interest,covariates Same as [parameter_glm()].
 #' @param model,vb,out_dir Same as [fit_learning_model()].
+#' @param fit_together Whether or not to fit all participant data to the model at the
+#' same time, or by group (if \code{FALSE}). Recommended for variational fits.
 #' @param iter_warmup_glm,iter_sampling_glm Number of warm-up and sampling iterations
 #' to fit Bayesian GLMs with (passed to [parameter_glm()]).
 #' @param min_blocks,max_blocks Minimum and maximum number of blocks to fit models on.
@@ -18,6 +20,7 @@ compare_block_diffs <- function(all_res,
                                 covariates,
                                 model,
                                 vb = TRUE,
+                                fit_together = vb,
                                 out_dir = "outputs/cmdstan/compare_blocks",
                                 iter_warmup_glm = 2000,
                                 iter_sampling_glm = 4000,
@@ -32,40 +35,89 @@ compare_block_diffs <- function(all_res,
   block_group <- trial_no <- NULL # to appease R CMD check
 
   rel_data <- list()
-  rel_data$ppt_info <- dplyr::bind_rows(
-    all_res[[1]]$ppt_info, all_res[[2]]$ppt_info
-  )
-  rel_data$training <- dplyr::bind_rows(
-    all_res[[1]]$training, all_res[[2]]$training
-  )
+  if (fit_together) {
+    rel_data$ppt_info <- dplyr::bind_rows(
+      all_res[[1]]$ppt_info, all_res[[2]]$ppt_info
+    )
+    rel_data$training <- dplyr::bind_rows(
+      all_res[[1]]$training, all_res[[2]]$training
+    )
+  } else {
+    rel_data <- all_res
+  }
 
   iter <- min_blocks:max_blocks
 
   par_df_ls <- list()
 
   for (i in seq_along(iter)) {
-    rel_data_tr <- rel_data
-    rel_data_tr$training <- rel_data_tr$training %>%
-      dplyr::filter(trial_no <= iter[i]*60)
-    fit_typ <- ifelse(vb, "vb", "mcmc")
-    first_n_blks <- fit_learning_model(
-      rel_data_tr, model = model, exp_part = "training", vb = vb,
-      out_dir = out_dir, ppc = FALSE, task_excl = l$task_excl,
-      accuracy_excl = l$accuracy_excl, model_checks = FALSE,
-      save_model_as = paste(
-        "first", i, "training_blocks", model, fit_typ, sep = "_"
-        ),
-      outputs = c("raw_df", "summary"), save_outputs = FALSE, ...
-    )
-    par_df_ls[[i]] <- parameter_glm(
-      summary_df = list(first_n_blks$summary),
-      raw_df = list(first_n_blks$raw_df),
-      var_of_interest = var_of_interest,
-      covariates = covariates,
-      iter_warmup = iter_warmup_glm,
-      iter_sampling = iter_sampling_glm,
-      ...
-    )
+    if (fit_together) {
+      rel_data_tr <- rel_data
+      rel_data_tr$training <- rel_data_tr$training %>%
+        dplyr::filter(trial_no <= iter[i]*60)
+
+      fit_typ <- ifelse(vb, "vb", "mcmc")
+
+      first_n_blks <- fit_learning_model(
+        rel_data_tr, model = model, exp_part = "training", vb = vb,
+        out_dir = out_dir, ppc = FALSE, task_excl = l$task_excl,
+        accuracy_excl = l$accuracy_excl, model_checks = FALSE,
+        save_model_as = paste(
+          "first", i, "training_blocks", model, fit_typ, sep = "_"
+          ),
+        outputs = c("raw_df", "summary"), save_outputs = FALSE, ...
+      )
+
+      par_df_ls[[i]] <- parameter_glm(
+        summary_df = list(first_n_blks$summary),
+        raw_df = list(first_n_blks$raw_df),
+        var_of_interest = var_of_interest,
+        covariates = covariates,
+        iter_warmup = iter_warmup_glm,
+        iter_sampling = iter_sampling_glm,
+        ...
+      )
+    }
+    else {
+      rel_data_gr1 <- rel_data[[1]]
+      rel_data_gr2 <- rel_data[[2]]
+
+      rel_data_gr1$training <- rel_data_gr1$training %>%
+        dplyr::filter(trial_no <= iter[i]*60)
+      rel_data_gr1$training <- rel_data_gr1$training %>%
+        dplyr::filter(trial_no <= iter[i]*60)
+      fit_typ <- ifelse(vb, "vb", "mcmc")
+      grp_names <- names(rel_data)
+
+      first_n_blks_gr1 <- fit_learning_model(
+        rel_data_gr1, model = model, exp_part = "training", vb = vb,
+        out_dir = out_dir, ppc = FALSE, task_excl = l$task_excl,
+        accuracy_excl = l$accuracy_excl, model_checks = FALSE,
+        save_model_as = paste(
+          "first", i, "training_blocks", model, grp_names[1], fit_typ, sep = "_"
+          ),
+        outputs = c("raw_df", "summary"), save_outputs = FALSE, ...
+      )
+      first_n_blks_gr2 <- fit_learning_model(
+        rel_data_gr2, model = model, exp_part = "training", vb = vb,
+        out_dir = out_dir, ppc = FALSE, task_excl = l$task_excl,
+        accuracy_excl = l$accuracy_excl, model_checks = FALSE,
+        save_model_as = paste(
+          "first", i, "training_blocks", model, grp_names[2], fit_typ, sep = "_"
+          ),
+        outputs = c("raw_df", "summary"), save_outputs = FALSE, ...
+      )
+
+      par_df_ls[[i]] <- parameter_glm(
+        summary_df = list(first_n_blks_gr1$summary, first_n_blks_gr2$summary),
+        raw_df = list(first_n_blk_gr1$raw_df, first_n_blk_gr2$raw_df),
+        var_of_interest = var_of_interest,
+        covariates = covariates,
+        iter_warmup = iter_warmup_glm,
+        iter_sampling = iter_sampling_glm,
+        ...
+      )
+    }
   }
 
   names_all <- c("Block 1 only", sapply(2:5, function(b) paste0("Block 1 to ", b)), "All blocks")
