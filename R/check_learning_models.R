760b2ed717a41355e9d9f40651a9ef4936961600
Quentin Dercon <quentindercon@gmail.com>
2022-01-28 14:51:41 +0000

    fix?

--- R/check_learning_models.R
+++ R/check_learning_models.R
@@ -138,7 +138,7 @@ check_learning_models <-
     for (p in seq_along(pars)) {
       dens_plts[[p]] <- dens_plot(
         mu_pars_df, nbins = 30, pars[p], col = pal[(p*2)-1], font = font,
-        font_size = font_size, alpha_par_nm = alpha_par_nm[p])
+        font_size = font_size, alpha_par_nm = alpha_par_nms[p])
     }
 
     ret$mu_par_dens <- cowplot::plot_grid(plotlist = dens_plts, nrow = 1)
