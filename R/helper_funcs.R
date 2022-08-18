ac5e8f4f0ed61e7fde407ea0ecf3ae73ca414090
Quentin Dercon <quentindercon@gmail.com>
2022-08-18 17:06:36 +0100

    various affect functions

--- R/helper_funcs.R
+++ R/helper_funcs.R
@@ -1,10 +1,22 @@
+#' Compute a single highest posterior density interval (HDI)
+#'
+#' \code{single_hdi} computes the highest density interval from a sample of
+#' representative values, estimated as shortest credible interval.
+#'
+#' @param vals A vector of representative values from a probability distribution
+#' (e.g., MCMC samples).
+#' @param cred A scalar between 0 and 1, indicating the mass within the
+#' credible interval that is to be estimated.
+#'
+#' @return A [base::vector()] with the lower and upper HDI.
 #' @noRd
-#' @keywords internal
 
-single_hdi <- function(vars,
+# Adapted from hBayesDM::HDIofMCMC, in turn based on John Kruschke's code.
+
+single_hdi <- function(vals,
                        cred) {
-  # adapted from hBayesDM::HDIofMCMC
-  sampleVec <- as.vector(t(vars))
+  # adapted from
+  sampleVec <- as.vector(t(vals))
   sortedPts = sort(sampleVec)
   ciIdxInc = floor(cred * length(sortedPts))
   nCIs = length(sortedPts) - ciIdxInc
@@ -17,6 +29,22 @@ single_hdi <- function(vars,
   HDIlim = c(HDImin , HDImax)
   return(as.vector(t(HDIlim)))
 }
+
+#' Compute quantiles of a probability distrbution based on highest density
+#' intervals (HDIs)
+#'
+#' \code{quantile_hdi} computes any number of highest density quantiles from a
+#' sample of representative values, estimated as shortest credible intervals.
+#'
+#' @param var A vector of representative values from a probability distribution
+#' (e.g., MCMC samples).
+#' @param quantile A vector of quantiles to return.
+#' @param transform Are values log-scaled (e.g., estimated from Gamma GLMs)?
+#'
+#' @return A sorted [base::vector()] with all HDIs plus the min/max/median if
+#' specified.
+#' @export
+
 quantile_hdi <- function(var,
                          quantile,
                          transform = FALSE) {
@@ -35,11 +63,11 @@ quantile_hdi <- function(var,
       returns <- cbind(returns, max(var))
     } else if (q < 0.5) {
       cred_mass = 1 - 2*q
-      HDI_lower <- single_hdi(vars = var, cred = cred_mass)[1]
+      HDI_lower <- single_hdi(vals = var, cred = cred_mass)[1]
       returns <- cbind(returns, HDI_lower)
     } else {
       cred_mass = 2*q - 1
-      HDI_upper <- single_hdi(vars = var, cred = cred_mass)[2]
+      HDI_upper <- single_hdi(vals = var, cred = cred_mass)[2]
       returns <- cbind(returns, HDI_upper)
     }
   }
@@ -53,10 +81,41 @@ quantile_hdi <- function(var,
 
   return(ret)
 }
+
+#' Define GLM family based on parameter name
+#'
+#' \code{family_ch} returns \code{Gamma(link = "log")} if the parameter is a
+#' learning rate (\code{alpha}) or decay factor (\code{gamma}), or
+#' \code{gaussian()} for any other parameter name.
+#'
+#' @param param Parameter name.
+#'
+#' @return A [stats::family()].
+#' @noRd
+
 family_ch <- function(param) {
   if (grepl("alpha|gamma", param)) return(Gamma(link = "log"))
   else return(gaussian())
 }
+
+#' Construct a tibble containing individual-level parameter values and baseline
+#' information
+#'
+#' \code{make_par_df} combines a [cmdstanr::summary()] with raw data to e.g.,
+#' pass to an outcome model, with filtering based on model fit metrics.
+#'
+#' @param raw Raw data, e.g., saved by [fit_learning_model()].
+#' @param summary A [cmdstanr::summary()] data frame.
+#' @param rhat_upper Upper bound of split r-hat values to include. Set to
+#' \code{Inf} to include all participants.
+#' @param ess_lower Lower bound of effective sample size values to include. Set
+#' to \code{0} to include all participants.
+#'
+#' @return A long format [tibble::tibble()] with model parameters and baseline
+#' participant data.
+#' @importFrom magrittr %>%
+#' @export
+
 make_par_df <- function(raw,
                         summary,
                         rhat_upper,
@@ -111,7 +170,27 @@ make_par_df <- function(raw,
 
   return(par_df)
 }
-axis_title <- function(param, p, test, alpha_par, alpha_par_nms) {
+
+#' Define axis title name
+#'
+#' \code{axis_title} returns an axis title, allowing for sub/superscripts and/or
+#' Greek letters.
+#'
+#' @param param Parameter name.
+#' @param p Iteration number.
+#' @param test,alpha_par Booleans indicating whether the parameter is from a
+#' test data and/or is a learning rate parameter.
+#' @param alpha_par_nms Names of learning rate parameters (ignored if
+#' \code{!alpha_par}).
+#'
+#' @return Axis title as a character string containing an expression.
+#' @noRd
+
+axis_title <- function(param,
+                       p,
+                       test,
+                       alpha_par,
+                       alpha_par_nms) {
   if (grepl("w", param, fixed = TRUE)) param <- sub("w", "w_", param)
   spl <- unlist(strsplit(param, "_"))
   s <- ifelse(test, paste0(spl[1], "*minute"), spl[1])
@@ -125,4 +204,122 @@ axis_title <- function(param, p, test, alpha_par, alpha_par_nms) {
     }
   } else a <- ""
   return(paste0(s, a))
+}
+
+#' Extract posterior predictions from affect data models and assess fit
+#'
+#' \code{get_affect_ppc} combines posterior predictions contained in a
+#' [posterior::draws_df()] outputted from a fit model with raw affect ratings,
+#' and returns various fit metrics (\eqn{R^2}, MAE, RMSE), for each individual.
+#'
+#' @param draws A [posterior::draws_df()]. Draws outputted from
+#' [fit_learning_model] (as a [posterior::draws_list()]) should be converted via
+#' [posterior::as_draws_df()] - this is memory intensive, hence it is not done
+#' internally.
+#' @param raw Raw data, e.g., saved by [fit_learning_model()].
+#' @param adj Name of the affect adjective - one of "happy", "confident" or
+#' "engaged".
+#'
+#' @return List containing a [base::data.frame()] with participant identifiers,
+#' numbers, and \eqn{R^2}, MAE and RMSE for each individual; and a named list
+#' (by ID) of data frames with individuals' mean posterior predictions and raw
+#' affect data.
+#' @importFrom magrittr %>%
+#' @importFrom data.table .SD as.data.table
+#' @export
+
+get_affect_ppc <- function(draws,
+                           raw,
+                           adj) {
+
+  draws <- as.data.table(draws)
+  indiv_ppcs <- list()
+
+  n_id <- length(unique(raw$subjID))
+  fit_df <- as.data.frame(
+    matrix(nrow = n_id, ncol = 5,
+           dimnames = list(1:n_id, c("subjID", "id_no", "R2", "MAE", "RMSE")))
+  )
+  pb = txtProgressBar(min = 0, max = n_id, initial = 0, style = 3)
+
+  for (i in 1:n_id) {
+    setTxtProgressBar(pb, i)
+    id <- unique(raw$subjID)[i]
+
+    affect_raw <- raw %>%
+      dplyr::filter(question_type == adj & subjID == id) %>%
+      dplyr::select(trial_no_q, question_response) %>%
+      dplyr::rename(mean_raw = question_response)
+
+    aff_tr <- which(raw[raw$subjID == id,]$question_type == adj)
+
+    affect_pred <-
+      draws[, .SD, .SDcols = patterns(paste0("^y_pred\\[", i, ","))]
+    affect_pred <- affect_pred[, ..aff_tr]
+
+    means <- colMeans(affect_pred)*100
+    se <- sapply(affect_pred, function(x) sd(x*100)/sqrt(length(x)))
+
+    pred_df <- tibble::tibble(
+      mean_pred = means, se_pred = se
+    ) %>%
+      dplyr::mutate(trial_no_q = dplyr::row_number())
+
+    obs <- affect_raw$mean_raw
+    pred <- pred_df$mean_pred
+
+    res_df <- affect_raw %>%
+      dplyr::left_join(pred_df, by = "trial_no_q") %>%
+      tidyr::pivot_longer(
+        cols = tidyselect::contains("mean"), names_to = "type",
+        names_prefix = "mean_"
+      )
+
+    fit_df[i,1] <- id
+    fit_df[i,2] <- i
+    fit_df[i,3] <- suppressWarnings(cor(obs, pred)^2) # some have variation = 0
+    fit_df[i,4] <- mean(abs(pred-obs))
+    fit_df[i,5] <- sqrt(mean((pred-obs)^2))
+
+    indiv_ppcs[[i]] <- res_df
+  }
+
+  names(indiv_ppcs) <- unique(raw$subjID)
+
+  ret <- list()
+  ret$fit_df <- fit_df
+  ret$indiv_ppcs <- indiv_ppcs
+
+  return(ret)
+}
+
+#' Extract individual-level weight parameters from affect data models
+#'
+#' \code{get_affect_wts} obtains weight parameters from affect data models, and
+#' names them with the appropriate affect adjective.
+#'
+#' @param summary A [cmdstanr::summary()].
+#' @param adj_order Same as [fit_learning_model()].
+#'
+#' @return A [tibble::tibble()] with weights by individual and adjective.
+#' @importFrom magrittr %>%
+#' @export
+
+get_affect_wts <- function(summary,
+                           adj_order = c("happy", "confident", "engaged")) {
+
+  summary %>%
+    dplyr::filter(grepl("w|gamma\\[", variable)) %>%
+    dplyr::filter(!grepl("mu|sigma", variable)) %>%
+    dplyr::select(variable, mean) %>%
+    dplyr::rowwise() %>%
+    dplyr::mutate(
+      par = unlist(strsplit(gsub("\\].*", "", variable), "\\[|,"))[1],
+      id_no = as.numeric(unlist(strsplit(gsub("\\].*", "", variable), "\\[|,"))[2]),
+      aff_num = as.numeric(unlist(strsplit(gsub("\\].*", "", variable), "\\[|,"))[3]),
+      adj = adj_order[aff_num]
+    ) %>%
+    dplyr::rename(post_mean = mean) %>%
+    dplyr::ungroup() %>%
+    dplyr::select(id_no, par, aff_num, post_mean, adj)
 }
---
+++ R/plot_affect.R
@@ -0,0 +1,141 @@
+#' Plot an individual's mean posterior predictions compared to their raw affect
+#' ratings
+#'
+#' \code{plot_affect_ppc} is capable of plotting either grouped or
+#' individual-level posterior predictions (vs. raw observations) for a defined
+#' list of posterior predictions and/or grouping.
+#'
+#' @param fit_list List of outputs from [get_affect_ppc].
+#' @param type Either \code{"individual"} or \code{"grouped"}.
+#' @param adj_order Same as [fit_learning_model()].
+#' @param nouns Formatted noun versions of the adjectives, in order.
+#' @param id_no If \code{type == "individual"}, a participant number to plot.
+#' If left as \code{NULL}, defaults to the individual with the median \eqn{R^2}
+#' for each adjective.
+#' @param r2_coords If \code{type == "individual"}, coordinates to print the
+#' \eqn{R^2} value.
+#' @param legend_pos,pal,font,font_size Same as [plot_import].
+#'
+#' @return A \code{list} of or single \code{ggplot} object(s) (if
+#' \code{type == "grouped"} and \code{length(adj_order) == 1}).
+#'
+#' @importFrom magrittr %>%
+#' @export
+
+plot_affect_ppc <- function(fit_list,
+                            type,
+                            adj_order = c("happy", "confident", "engaged"),
+                            nouns = c("Happiness", "Confidence", "Engagement"),
+                            id_no = NULL,
+                            r2_coords = c(0.9, 0.8),
+                            legend_pos = "right",
+                            pal = NULL,
+                            font = "",
+                            font_size = 11) {
+
+  if(is.null(pal)) pal <- c("#ffc9b5", "#95a7ce", "#987284")
+
+  if (type == "grouped") {
+    ppc_list <- lapply(
+      1:length(adj_order),
+      function(f) {
+        dplyr::mutate(
+          data.table::rbindlist(
+            fit_list[[f]]$indiv_ppcs, idcol = "subjID"
+          ),
+          adj = adj_order[f]
+        )
+      }
+    )
+
+    data.table::rbindlist(ppc_list) %>%
+      dplyr::group_by(adj, type, trial_no_q) %>%
+      dplyr::mutate(mean_val = mean(value), se_val = std(value)) %>%
+      dplyr::distinct(trial_no_q, adj, type, mean_val, se_val) %>%
+      ggplot2::ggplot(
+        ggplot2::aes(
+          x = trial_no_q, y = mean_val, color = adj, fill = adj,
+          linetype = factor(type, levels = c("raw", "pred")))
+      ) +
+      ggplot2::scale_x_continuous(limits = c(0, 120), breaks = seq(0, 120, 20)) +
+      ggplot2::scale_y_continuous(limits = c(25, 72)) +
+      ggplot2::geom_line(size = 1.1, alpha = 0.5) +
+      ggplot2::geom_ribbon(
+        ggplot2::aes(
+          ymin = mean_val - se_val, ymax = mean_val + se_val), alpha = 0.3, colour = NA
+      ) +
+      ggplot2::scale_color_manual(
+        name = NULL,
+        values = pal
+      ) +
+      ggplot2::scale_fill_manual(
+        name = NULL,
+        values = pal
+      ) +
+      ggplot2::xlab("Trial number") +
+      ggplot2::ylab(paste0("Mean (± SE) affect rating")) +
+      ggplot2::guides(linetype = "none", color = "none", fill = "none") +
+      cowplot::theme_half_open(
+        font_size = font_size,
+        font = font
+      ) +
+      ggplot2::theme(legend.position = c(0.85, 0.85))
+  }
+
+  median_id <- function(df, type, id = NULL) {
+    if (type == "num") {
+      subset(df, R2 == quantile(df$R2, p = 0.5, type = 1, na.rm = T))$id_no
+    } else if (type == "id" & !is.null(id)) {
+      subset(df, id_no == id)$subjID
+    }
+  }
+
+  len <- length(fit_list)
+  id_vec <- vector(mode = "integer", length = len)
+
+  if (is.null(id_no)) {
+    id_vec <- sapply(1:len, function(f) median_id(fit_list[[f]]$fit_df, "num"))
+  } else {
+    id_vec <- rep(id_no, len)
+  }
+
+  plots <- list()
+
+  for (a in 1:length(adj_order)) {
+    adj <- adj_order[a]
+    r2 <- subset(fit_list[[a]]$fit_df, id_no == id_vec[a])$R2
+
+    plots[[adj]] <-
+      fit_list[[a]]$indiv_ppcs[[
+        median_id(fit_list[[a]]$fit_df, "id", id_vec[a])
+      ]] %>%
+      ggplot2::ggplot(
+        ggplot2::aes(x = trial_no_q, y = value, color = type, fill = type)
+      ) +
+      ggplot2::geom_line() +
+      ggplot2::geom_ribbon(
+        ggplot2::aes(ymin = value - se_pred, ymax = value + se_pred),
+        alpha = 0.5
+      ) +
+      ggplot2::scale_color_manual(
+        name = "Data type", labels = c("Predicted", "Real"),
+        values = c(pal[a*2-1], pal[a*2])
+      ) +
+      ggplot2::scale_fill_manual(
+        name = "Data type", labels = c("Predicted", "Real"),
+        values = c(pal[a*2-1], pal[a*2])
+      ) +
+      ggplot2::xlab("Trial number") +
+      ggplot2::ylab(paste0(nouns[a], " rating /100")) +
+      cowplot::theme_half_open(font_size = font_size, font = font) +
+      ggplot2::annotation_custom(
+        grid::textGrob(
+          bquote(R^2~"="~.(round(r2, 3))),
+          gp = grid::gpar(fontsize = 16, col = "steelblue4"),
+          x = r2_coords[1], y = r2_coords[2]
+        )
+      )  +
+      ggplot2::theme(legend.position = legend_pos)
+  }
+  return(plots)
+}
